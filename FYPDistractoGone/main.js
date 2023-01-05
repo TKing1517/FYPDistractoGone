5a72bf9897bbff34b5c21768fb5a02e6ea5de4de
Tashfeen Janjua <tjanjua115@gmail.com>
2023-01-05 18:44:29 +0000

    does it block?

--- FYPDistractoGone/main.js
+++ FYPDistractoGone/main.js
@@ -1,13 +1,34 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
+const { exec } = require('child_process')
+
+
+
+
 const createWindow = () => {
-    const win = new BrowserWindow({
-      width: 800,
-      height: 600
-    })
-  
-    win.loadFile('Screens/index.html')
-    win.setMenu(null)
-  }
-  app.whenReady().then(() => {
-    createWindow()
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
   })
+
+  win.loadFile('Screens/index.html')
+  blockWebsite('https://onesquareminesweeper.com/')
+  blockWebsite('onesquareminesweeper.com')
+  blockWebsite('www.onesquareminesweeper.com')
+  win.setMenu(null)
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
+
+
+const blockWebsite = (website) => {
+  const command = `echo 127.0.0.1 ${website} >> C:\\Windows\\System32\\drivers\\etc\\hosts`
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error: ${error}`)
+      return
+    }
+    console.log(`Success: ${stdout}`)
+  })
+}
