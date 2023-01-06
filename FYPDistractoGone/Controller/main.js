908ba556fe1934484b1070e0ba520ca424667623
Tashfeen Janjua <tjanjua115@gmail.com>
2023-01-06 19:25:30 +0000

    user can input and remove websites then start blok

--- FYPDistractoGone/Controller/main.js
+++ FYPDistractoGone/Controller/main.js
@@ -2,6 +2,9 @@ const { app, BrowserWindow, ipcMain } = require('electron')
 const { exec } = require('child_process')
 const fs = require('fs')
 
+
+let websitesURLs = [];
+
 const createWindow = () => {
   const win = new BrowserWindow({
     webPreferences: {
@@ -11,8 +14,10 @@ const createWindow = () => {
     }
   })
 
-  win.loadFile('View/HomePage.html')
   
+
+  win.loadFile('View/HomePage.html')
+  // unblockWebsite('om')
   win.webContents.openDevTools();
   win.setMenu(null)
 }
@@ -22,46 +27,93 @@ app.whenReady().then(() => {
 })
 
 ipcMain.on('submit-website', (event, website) => {
-  console.log(website)
-  blockWebsite(website)
+  const hostname = new URL(website).hostname;
+  websitesURLs.push(hostname);
+  console.log(websitesURLs)
+
+})
+
+ipcMain.on('submit-websiteU', (event, website) => {
+  const hostname = new URL(website).hostname;
+  websitesURLs = websitesURLs.filter((item) => item !== hostname);
+  console.log(websitesURLs)
+
+})
+
+ipcMain.on('BeginRestriction', (event, input) => {
+  console.log(input)
+  console.log(websitesURLs)
+  blockWebsite(websitesURLs)
+  const timeoutId = setTimeout(function() {
+    unblockWebsite(websitesURLs)
+  }, 30000);
 })
 
 
 const blockWebsite = (website) => {
-  const command = `echo 127.0.0.1 ${website} >> C:\\Windows\\System32\\drivers\\etc\\hosts`
-  exec(command, (error, stdout, stderr) => {
+
+//   // Close all Chrome windows
+// exec('taskkill /im chrome.exe /f', (error, stdout, stderr) => {
+//   if (error) {
+//     console.error(`${error}`);
+//     return;
+//   }
+//   console.log(`Success: ${stdout}`);
+// });
+
+// const { exec } = require('child_process');
+
+// // Close all Firefox windows
+// exec('taskkill /im firefox.exe /f', (error, stdout, stderr) => {
+//   if (error) {
+//     console.error(`${error}`);
+//     return;
+//   }
+//   console.log(`Success: ${stdout}`);
+// });
+
+  const command = `echo 127.0.0.1 `
+  for (let i = 0; i < website.length; i++) {
+  exec(command + website[i] + ` >> C:\\Windows\\System32\\drivers\\etc\\hosts`, (error, stdout, stderr) => {
     if (error) {
       console.error(`${error}`)
       return
     }
     console.log(`Success: ${stdout}`)
   })
+  }
 }
 
-const unblockWebsite = (website) => {
+const unblockWebsite = (websites) => {
+  // Check if websites is an array
+  if (!Array.isArray(websites)) {
+    console.error('Error: websites must be an array');
+    return;
+  }
+
   // Read content
   fs.readFile('C:\\Windows\\System32\\drivers\\etc\\hosts', 'utf8', (error, data) => {
     if (error) {
-      console.error(`Error: ${error}`)
-      return
+      console.error(`Error: ${error}`);
+      return;
     }
 
     // Split contents on new line
-    const lines = data.split('\n')
+    const lines = data.split('\n');
 
-    // Find lines that contain website
-    const filteredLines = lines.filter((line) => !line.includes(website))
+    // Find lines that contain any of the websites
+    const filteredLines = lines.filter((line) => !websites.some((website) => line.includes(website)));
 
     // Join split lines into string
-    const modifiedData = filteredLines.join('\n')
+    const modifiedData = filteredLines.join('\n');
 
     // Write back to hosts file
     fs.writeFile('C:\\Windows\\System32\\drivers\\etc\\hosts', modifiedData, (error) => {
       if (error) {
-        console.error(`${error}`)
-        return
+        console.error(`${error}`);
+        return;
       }
-      console.log('Success: website unblocked')
-    })
-  })
-}
+      console.log('Success: websites unblocked');
+    });
+  });
+};
--- FYPDistractoGone/Controller/renderer.js
+++ FYPDistractoGone/Controller/renderer.js
@@ -1,7 +1,22 @@
 const { ipcRenderer } = require('electron')
-const form = document.getElementById('SubmitWebsiteInput')
-form.addEventListener('submit', (event) => {
+const SubmitWebsiteInput = document.getElementById('SubmitWebsiteInput')
+SubmitWebsiteInput.addEventListener('submit', (event) => {
     event.preventDefault()
     const input = document.getElementById('InputWebsite').value
     ipcRenderer.send('submit-website', input)
 })
+
+const DeleteWebsiteInput = document.getElementById('DeleteWebsiteInput')
+DeleteWebsiteInput.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const input = document.getElementById('InputWebsiteU').value
+    ipcRenderer.send('submit-websiteU', input)
+})
+
+const BeginRestriction = document.getElementById('BeginRestriction')
+BeginRestriction.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const input = "document.getElementById('InputWebsiteU').value"
+    ipcRenderer.send('BeginRestriction', input)
+})
+
